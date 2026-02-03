69dfaa8b1335794b05f87c7b339d22afdd9860ba
DaffaRomyz <daffaromyzaufa@gmail.com>
2026-02-03 11:58:30 +0700

    add same currency conversion

--- srv/src/main/java/customer/cap_btp_job/util/CurrencyExchange.java
+++ srv/src/main/java/customer/cap_btp_job/util/CurrencyExchange.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class CurrencyExchange {
 
-    private static enum Currency {
+    public static enum Currency {
         USD,
         IDR,
         CNY
@@ -17,13 +17,16 @@ public class CurrencyExchange {
     // "CNYIDR", 2500d,
     // "IDRUSD", 0.0000625d,
     // "IDRCNY", 0.00040d,
-    private static Map<Currency, Map<Currency, Double>> exchangeRates = Map.of(
+    public static Map<Currency, Map<Currency, Double>> exchangeRates = Map.of(
         Currency.USD, Map.of(Currency.IDR, 16000d, Currency.CNY, 6.4d),
         Currency.CNY, Map.of(Currency.USD, 0.15625d, Currency.IDR, 2500d),
         Currency.IDR, Map.of(Currency.USD, 0.0000625d, Currency.CNY, 0.00040d)
     );
     
     public static Double convertCurrency(Double amount, Currency currencyFrom, Currency currencyTo) {
+        if (currencyFrom.compareTo(currencyTo) == 0) {
+            return amount;
+        }
         return amount * exchangeRates.get(currencyFrom).get(currencyTo);
     }
 }
